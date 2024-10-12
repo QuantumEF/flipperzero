9f6056940fd5b25d2c7cafa3f6b042355e9d815a
Erik Fong <quantumerikfong@gmail.com>
2024-10-11 20:10:57 -0400

    Added a builder class, incomplete as it has no methods for adding commands to the buffer.

--- crates/flipperzero/src/subghz/preset.rs
+++ crates/flipperzero/src/subghz/preset.rs
@@ -1,5 +1,5 @@
-// #[cfg(feature = "alloc")]
-// use alloc::vec::Vec;
+#[cfg(feature = "alloc")]
+use alloc::vec::Vec;
 use flipperzero_sys::{
     FuriHalSubGhzPreset, FuriHalSubGhzPreset_FuriHalSubGhzPreset2FSKDev238Async,
     FuriHalSubGhzPreset_FuriHalSubGhzPreset2FSKDev476Async,
@@ -11,7 +11,6 @@ use flipperzero_sys::{
     FuriHalSubGhzPreset_FuriHalSubGhzPresetOok650Async,
 };
 
-// #[cfg(feature = "alloc")]
 #[derive(Clone, Debug)]
 pub enum SubGhzPreset<'a> {
     /// default configuration
@@ -55,22 +54,22 @@ impl<'a> SubGhzPreset<'a> {
 pub struct CustomSubGhzPreset<'a>(&'a [u8]);
 
 impl<'a> CustomSubGhzPreset<'a> {
-    // #[cfg(feature = "alloc")]
-    // pub fn builder() -> CustomSubGhzPresetBuilder {
-    //     CustomSubGhzPresetBuilder {
-    //         data: Vec::new(),
-    //         pa_table: [0; 8],
-    //     }
-    // }
+    #[cfg(feature = "alloc")]
+    pub fn builder() -> CustomSubGhzPresetBuilder {
+        CustomSubGhzPresetBuilder {
+            data: Vec::new(),
+            pa_table: [0; 8],
+        }
+    }
 
-    // #[cfg(feature = "alloc")]
-    // /// TODO: best way to describe the capacity variable?
-    // pub fn builder_with_capacity(capacity: usize) -> CustomSubGhzPresetBuilder {
-    //     CustomSubGhzPresetBuilder {
-    //         data: Vec::with_capacity(capacity + 8),
-    //         pa_table: [0; 8],
-    //     }
-    // }
+    #[cfg(feature = "alloc")]
+    /// TODO: best way to describe the capacity variable?
+    pub fn builder_with_capacity(capacity: usize) -> CustomSubGhzPresetBuilder {
+        CustomSubGhzPresetBuilder {
+            data: Vec::with_capacity(capacity + 8),
+            pa_table: [0; 8],
+        }
+    }
 
     /// # Safety
     /// Read the manual for the CC1101 chip and review the [`furi_hal_subghz_load_custom_preset()`](https://github.com/flipperdevices/flipperzero-firmware/blob/0eaad8bf64f01a6f932647a9cda5475dd9ea1524/targets/f7/furi_hal/furi_hal_subghz.c#L162) function provided by the flipper API
@@ -83,19 +82,19 @@ impl<'a> CustomSubGhzPreset<'a> {
     }
 }
 
-// #[cfg(feature = "alloc")]
-// pub struct CustomSubGhzPresetBuilder {
-//     data: Vec<u8>,
-//     pa_table: [u8; 8],
-// }
+#[cfg(feature = "alloc")]
+pub struct CustomSubGhzPresetBuilder {
+    data: Vec<u8>,
+    pa_table: [u8; 8],
+}
 
-// impl CustomSubGhzPresetBuilder {
-//     pub fn build(mut self) -> CustomSubGhzPreset {
-//         // Null Terminate the register/data pairs.
-//         self.data.extend_from_slice(&[0, 0]);
+impl CustomSubGhzPresetBuilder {
+    pub fn build(&mut self) -> CustomSubGhzPreset<'_> {
+        // Null Terminate the register/data pairs.
+        self.data.extend_from_slice(&[0, 0]);
 
-//         self.data.extend_from_slice(&self.pa_table);
+        self.data.extend_from_slice(&self.pa_table);
 
-//         CustomSubGhzPreset(self.data)
-//     }
-// }
+        CustomSubGhzPreset(&self.data)
+    }
+}
